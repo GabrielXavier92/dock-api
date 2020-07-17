a2ed45961a2309541f4cfc56833aa236ecc5065b
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-17 16:29:50 -0300

    add comments

--- src/app/entities/people/service/peopleService.ts
+++ src/app/entities/people/service/peopleService.ts
@@ -3,6 +3,8 @@ import PeopleModel from '../model/peopleModel';
 
 class PeopleService implements InterfacePeopleService {
   public async createPeople({ name, cpf, birthDate }: PeopleInput): Promise<People> {
+    // Poderia ser adicionado mais regras de negocio nessa camada como verificacao de documento
+    // verificacao da idade, entre outros
     const birthDateRegex = new RegExp(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/);
 
     if (!birthDateRegex.test(birthDate)) {
