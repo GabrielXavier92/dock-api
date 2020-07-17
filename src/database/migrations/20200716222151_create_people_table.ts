4383760b9b57b9e4f31a91c6e2431f4aace97f06
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-16 23:08:48 -0300

    create people table migrations and seed

---
+++ src/database/migrations/20200716222151_create_people_table.ts
@@ -0,0 +1,15 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.raw(`
+    CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    cpf text NOT NULL,
+    birthDate DATE NOT NULL
+  );
+`);
+}
+
+export async function down(): Promise<void> {
+}
---
+++ src/database/seeds/create_people.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import * as Knex from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+  return knex.raw(`
+    INSERT INTO people (name, cpf, birthDate)
+    VALUES('Dock API', '12314322112', '1992-04-12')
+  `);
+}
