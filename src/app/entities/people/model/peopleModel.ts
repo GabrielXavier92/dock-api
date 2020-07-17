b7144659661cebb46bb23d36c9e0da05e8a95264
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-17 00:55:53 -0300

    create people model

--- src/app/entities/people/model/peopleModel.ts
+++ src/app/entities/people/model/peopleModel.ts
@@ -1,13 +1,15 @@
+import connection from '../../../../database/connection';
+
 import { People, PeopleInput, InterfacePeopleModel } from '../people.interface';
 
 class PeopleModel implements InterfacePeopleModel {
-  public create({ name, birthDate, cpf }: PeopleInput): People {
-    // insere no bacno
-    const createdPeople: People = {
-      idPeople: 1234, name, cpf, birthDate,
-    };
-
-    return createdPeople;
+  public async create({ name, birthDate, cpf }: PeopleInput): Promise<People> {
+    const db = await connection.raw(`
+      INSERT INTO people (name, cpf, birthDate)
+      VALUES(?, ?, ?)
+      RETURNING 0
+    `, [`${name}`, `${cpf}`, `${birthDate}`]);
+    return db.rows[0] as People;
   }
 }
 
