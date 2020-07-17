14697b4c572c4437f2312c3b141755868457798b
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-17 16:30:17 -0300

    change test

--- src/tests/people/service/peopleService.spec.ts
+++ src/tests/people/service/peopleService.spec.ts
@@ -1,46 +1,52 @@
-import { InterfacePeopleModel } from '../../../app/entities/people/people.interface.d';
 import PeopleService from '../../../app/entities/people/service/peopleService';
 import PeopleModel from '../../../app/entities/people/model/peopleModel';
 
 import { InterfacePeopleService } from '../../../app/entities/people/people.interface';
 
-jest.mock('../../../app/entities/people/model/peopleModel');
-
 type maker = {
   peopleService: InterfacePeopleService;
-  peopleModel: InterfacePeopleModel;
 };
 const maker = (): maker => {
   const peopleService = PeopleService;
-  const peopleModel = PeopleModel;
-  return { peopleService, peopleModel };
+  return { peopleService };
 };
 
-describe('createPeople', () => {
+describe('peopleService', () => {
   beforeEach(() => {
     jest.resetModules();
   });
 
-  test('Should throw new error if date is invalid', async () => {
-    const { peopleService } = maker();
-    const people = {
-      name: 'Gabriel Xavier',
-      birthDate: '1992-04-1',
-      cpf: '123213',
-    };
-    await expect(peopleService.createPeople(people)).rejects.toThrow();
-    await expect(peopleService.createPeople(people)).rejects.toThrowError('Invalid Input');
-  });
+  describe('createPeople', () => {
+    test('Should throw new error if date is invalid', async () => {
+      const { peopleService } = maker();
+      const people = {
+        name: 'Gabriel Xavier',
+        birthDate: '1992-04-1',
+        cpf: '123213',
+      };
+      await expect(peopleService.createPeople(people)).rejects.toThrow();
+      await expect(peopleService.createPeople(people)).rejects.toThrowError('Invalid Input');
+    });
 
-  test('Should return people if date is valid', async () => {
-    const { peopleService, peopleModel } = maker();
-    const people = {
-      name: 'Gabriel Xavier',
-      birthDate: '1992-04-12',
-      cpf: '123213',
-    };
-    await peopleService.createPeople(people);
+    test('Should return people if data is valid', async () => {
+      const { peopleService } = maker();
+      const people = {
+        name: 'Gabriel Xavier',
+        birthDate: '1992-04-12',
+        cpf: '123213',
+      };
 
-    expect(peopleModel.create).toBeCalledTimes(1);
+      const createdPeople = {
+        idPeople: 123,
+        ...people,
+      };
+
+      jest.spyOn(PeopleModel, 'create').mockResolvedValue(createdPeople);
+
+      const service = await peopleService.createPeople(people);
+
+      expect(PeopleModel.create).toBeCalledTimes(1);
+      expect(service).toBe(createdPeople);
+    });
   });
 });
