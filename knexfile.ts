37d6b31ccd0774d79b8f57f43581c63777275de9
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-16 23:07:53 -0300

    create knex file

---
+++ knexfile.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+
+module.exports = {
+  client: 'pg',
+  connection: process.env.DATABASE_FULL_URL || {
+    host: process.env.DATABASE_URL,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './src/database/migrations'
+  },
+  seeds: {
+    directory: './src/database/seeds'
+  }
+
+};
