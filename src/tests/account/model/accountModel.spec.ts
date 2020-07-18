108a2cca4c3d3e1dc32efa52a81bb319c3466e20
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 11:46:46 -0300

    test when create account throw an error

--- src/tests/account/model/accountModel.spec.ts
+++ src/tests/account/model/accountModel.spec.ts
@@ -4,7 +4,7 @@ import AccountModel from '../../../app/entities/account/model/accountModel';
 
 import connecetion from '../../../database/connection';
 
-import { InterfaceAccountModel } from "../../../app/entities/account/account.interface.d";
+import { InterfaceAccountModel } from '../../../app/entities/account/account.interface.d';
 
 jest.mock('../../../database/connection');
 
@@ -24,6 +24,23 @@ describe('accountModel', () => {
   });
 
   describe('create', () => {
+    test('Shoud throw an error case create account failed', async () => {
+      const { conn, accountModel } = maker();
+      const account = {
+        idPeople: 123,
+        balance: 50,
+        dailyWithdrawalLimit: 150,
+        active: true,
+        accountType: 1,
+        createdAt: new Date().toISOString().slice(0, 10),
+      };
+
+      jest.spyOn(conn, 'raw').mockRejectedValue('Internal Error');
+
+      await expect(accountModel.create(account)).rejects.toThrow();
+      await expect(accountModel.create(account)).rejects.toThrowError('Internal Error');
+    });
+
     test('Should return created account', async () => {
       const { conn, accountModel } = maker();
       const account = {
@@ -32,18 +49,89 @@ describe('accountModel', () => {
         dailyWithdrawalLimit: 150,
         active: true,
         accountType: 1,
+        createdAt: new Date().toISOString().slice(0, 10),
       };
 
       const createdAccount = {
         idAccount: 1,
         ...account,
-        createdAt: new Date().toISOString().slice(0, 10),
       };
 
       const raw = jest.spyOn(conn, 'raw').mockResolvedValue({ rows: [{ ...createdAccount }] });
-      const createdPeople = await accountModel.create(account);
+      const create = await accountModel.create(account);
       expect(raw).toBeCalledTimes(1);
-      expect(createdPeople).toEqual({ ...createdAccount });
+      expect(create).toEqual({ ...createdAccount });
+    });
+  });
+
+  describe('update', () => {
+    test('Shoud throw an error case update account failed', async () => {
+      const { conn, accountModel } = maker();
+      const account = {
+        idAccount: 1,
+        idPeople: 123,
+        balance: 50,
+        dailyWithdrawalLimit: 150,
+        active: true,
+        accountType: 1,
+        createdAt: new Date().toISOString().slice(0, 10),
+      };
+
+      jest.spyOn(conn, 'raw').mockRejectedValue('Internal Error');
+
+      await expect(accountModel.updateById(1, account)).rejects.toThrow();
+      await expect(accountModel.updateById(1, account)).rejects.toThrowError('Internal Error');
+    });
+
+    test('Should return updated account', async () => {
+      const { conn, accountModel } = maker();
+      const account = {
+        idAccount: 1,
+        idPeople: 123,
+        balance: 50,
+        dailyWithdrawalLimit: 150,
+        active: true,
+        accountType: 1,
+        createdAt: new Date().toISOString().slice(0, 10),
+      };
+
+      const updatedAccount = {
+        ...account,
+        active: false,
+      };
+
+      const raw = jest.spyOn(conn, 'raw').mockResolvedValue({ rows: [{ ...updatedAccount }] });
+      const update = await accountModel.updateById(1, account);
+      expect(raw).toBeCalledTimes(1);
+      expect(update).toEqual({ ...updatedAccount });
+    });
+  });
+
+  describe('findById', () => {
+    test('Shoud throw an error case findById account failed', async () => {
+      const { conn, accountModel } = maker();
+      jest.spyOn(conn, 'raw').mockRejectedValue('Internal Error');
+
+      await expect(accountModel.findById(1)).rejects.toThrow();
+      await expect(accountModel.findById(1)).rejects.toThrowError('Internal Error');
+    });
+
+    test('Should return a people', async () => {
+      const { conn, accountModel } = maker();
+      const account = {
+        idAccount: 1,
+        idPeople: 123,
+        balance: 50,
+        dailyWithdrawalLimit: 150,
+        active: true,
+        accountType: 1,
+        createdAt: new Date().toISOString().slice(0, 10),
+      };
+
+      const raw = jest.spyOn(conn, 'raw').mockResolvedValue({ rows: [{ ...account }] });
+      const createdPeople = await accountModel.findById(1);
+      expect(raw).toBeCalledTimes(1);
+      expect(createdPeople).toEqual({ ...account });
     });
   });
 });
