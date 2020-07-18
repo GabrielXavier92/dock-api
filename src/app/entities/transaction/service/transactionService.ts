ebaf12faefb228ae2b076cd64609a329a9375a93
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 20:13:23 -0300

    create validate method

--- src/app/entities/transaction/service/transactionService.ts
+++ src/app/entities/transaction/service/transactionService.ts
@@ -1,4 +1,6 @@
-import { InterfaceTransactionService, TransactionInput, Transaction } from '../transaction.interface';
+import {
+  InterfaceTransactionService, TransactionInput, Transaction, TransactionValidate,
+} from '../transaction.interface';
 import accountService from '../../account/service/accountService';
 import transactionModel from '../model/transactionModel';
 import { AccountInput } from '../../account/account.interface';
@@ -6,46 +8,39 @@ import checkDecimal from '../../../../utils/checkDecimal';
 
 class TransactionService implements InterfaceTransactionService {
   public async deposit(transaction: TransactionInput): Promise<Transaction> {
-    const { idAccount, value } = transaction;
-
-    if (value < 0) throw new Error('Value most be bigger than 0');
-    if (!checkDecimal(value)) throw new Error('Invalid value');
-
-    const account = await accountService.getAccount(idAccount);
-    if (!account.active) throw new Error('Blocked Account');
-
-    const newTransaction = await transactionModel.create(transaction);
-
+    const { account, newTransaction } = await this.validate(transaction);
     const newAccount: AccountInput = {
       ...account,
       balance: Number(account.balance) + Number(newTransaction.value),
     };
-
-    await accountService.updateAccountById(idAccount, newAccount);
-
+    await accountService.updateAccountById(transaction.idAccount, newAccount);
     return newTransaction;
   }
 
   public async withdraw(transaction: TransactionInput): Promise<Transaction> {
-    const { idAccount, value } = transaction;
-
-    if (value < 0) throw new Error('Value most be bigger than 0');
-    if (!checkDecimal(value)) throw new Error('Invalid value');
-
-    const account = await accountService.getAccount(idAccount);
-    if (account.balance < value) throw new Error('Insufficient funds');
-    if (!account.active) throw new Error('Blocked Account');
-
-    const newTransaction = await transactionModel.create(transaction);
+    const { account, newTransaction } = await this.validate(transaction);
+    if (account.balance < transaction.value) throw new Error('Insufficient funds');
 
     const newAccount: AccountInput = {
       ...account,
       balance: Number(account.balance) - Number(newTransaction.value),
     };
-
-    await accountService.updateAccountById(idAccount, newAccount);
-
+    await accountService.updateAccountById(transaction.idAccount, newAccount);
     return newTransaction;
+  }
+
+  private async validate(transaction: TransactionInput): Promise<TransactionValidate> {
+    const { idAccount, value } = transaction;
+
+    if (value < 0) throw new Error('Value most be bigger than 0');
+    if (!checkDecimal(value)) throw new Error('Invalid value');
+
+    const account = await accountService.getAccount(idAccount);
+    if (!account.active) throw new Error('Blocked Account');
+
+    const newTransaction = await transactionModel.create(transaction);
+
+    return { account, newTransaction };
   }
 }
 
