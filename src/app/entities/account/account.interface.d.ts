ec94117cbe98b7af90e3f2bdb6b10fc7693211a0
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 12:54:21 -0300

    add new methods to account interface

--- src/app/entities/account/account.interface.d.ts
+++ src/app/entities/account/account.interface.d.ts
@@ -24,18 +24,17 @@ export interface AccountModelInput extends AccountInput {
 export interface InterfaceAccountController {
   createAccount: (req: Request, res: Response) => Promise<void>;
   blockAccount: (req: Request, res: Response) => Promise<void>;
-  // balanceAccount: (req: Request, res: Response) => Promise<void>;
+  getAccount: (req: Request, res: Response) => Promise<void>;
 }
 
 export interface InterfaceAccountService {
   createAccount: (account: AccountInput) => Promise<Account>;
   blockAccount: (idAccount: number) => Promise<Account>;
-  // balanceAccount: (idAccount: number) => Promise<Account>;
+  getAccount: (idAccount: number) => Promise<Account>;
 }
 
 export interface InterfaceAccountModel {
   create: (account: AccountModelInput) => Promise<Account>;
   updateById: (id: number, account: AccountModelInput) => Promise<Account>;
   findById: (idAccount: number) => Promise<Account | undefined>;
-  // updateById: (idAccount: number, account: AccountInput) => Promise<Account>;
 }
