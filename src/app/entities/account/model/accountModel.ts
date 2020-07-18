029f2f41689140be42ae7c1da1ae84cc79480a1b
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 12:23:16 -0300

    creat finById and updateById account methods

--- src/app/entities/account/model/accountModel.ts
+++ src/app/entities/account/model/accountModel.ts
@@ -20,6 +20,41 @@ class AccountModel implements InterfaceAccountModel {
       throw new Error('Internal Error');
     }
   }
+
+  public async updateById(id: number, account: AccountModelInput): Promise<Account> {
+    const {
+      idPeople, balance, dailyWithdrawalLimit, active, accountType,
+    } = account;
+
+    try {
+      const db = await connection.raw(`
+        UPDATE account 
+        SET "idPeople"=?, 
+            balance=?,
+            "dailyWithdrawalLimit"=?,
+            active=?,
+            "accountType"=?, 
+        WHERE "idAccount"=?,
+        RETURNING *;
+      `, [`${idPeople}`, `${balance}`, `${dailyWithdrawalLimit}`, `${active}`, `${accountType}`, `${id}`]);
+
+      return db.rows[0] as Account;
+    } catch (e) {
+      throw new Error('Internal Error');
+    }
+  }
+
+  public async findById(id: number): Promise<Account | undefined> {
+    try {
+      const db = await connection.raw(`
+      SELECT * FROM "account" where "idAccount"=?
+    `, [id]);
+
+      return db.rows[0] as Account;
+    } catch (e) {
+      throw new Error('Internal Error');
+    }
+  }
 }
 
 export default new AccountModel();
