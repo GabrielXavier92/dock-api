04287b52fd3e06e7772a9b6ae71d8a2a4bcd987c
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 13:26:15 -0300

    migration to create transaction table

---
+++ src/database/migrations/20200718130136_create_transactions_table.ts
@@ -0,0 +1,17 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.raw(`
+    CREATE TABLE "transaction" (
+    "idTransaction" SERIAL PRIMARY KEY,
+    "idAccount" INT NOT NULL,
+    value NUMERIC NOT NULL,
+    "createdAt" DATE NOT NULL,
+    
+    FOREIGN KEY ("idAccount") REFERENCES account("idAccount")
+    );
+`);
+}
+
+export async function down(): Promise<void> {
+}
