af24c0bb20ae62348da1e50dbe898cff539b2c1e
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 19:26:10 -0300

    create validate method

--- src/app/entities/transaction/controller/transactionController.ts
+++ src/app/entities/transaction/controller/transactionController.ts
@@ -1,14 +1,11 @@
 import * as express from 'express';
-import { InterfaceTransactionController } from '../transaction.interface';
+import { InterfaceTransactionController, TransactionInput } from '../transaction.interface';
 import transactionService from '../service/transactionService';
 
 class TransactionController implements InterfaceTransactionController {
   public async deposit(req: express.Request, res: express.Response): Promise<void> {
-    const { value } = req.body;
-    const { idAccount } = req.params;
     try {
-      if (!idAccount || !value) throw new Error('Invalid Input');
-      if (typeof value !== 'number') throw new Error('Invalid Input');
+      const { idAccount, value } = this.validate(req);
 
       const transaction = await transactionService.deposit({ idAccount: Number(idAccount), value });
 
@@ -19,18 +16,23 @@ class TransactionController implements InterfaceTransactionController {
   }
 
   public async withdraw(req: express.Request, res: express.Response): Promise<void> {
-    const { value } = req.body;
-    const { idAccount } = req.params;
     try {
-      if (!idAccount || !value) throw new Error('Invalid Input');
-      if (typeof value !== 'number') throw new Error('Invalid Input');
-
-      const transaction = await transactionService.withdraw({ idAccount: Number(idAccount), value });
+      const { idAccount, value } = this.validate(req);
+      const transaction = await transactionService.withdraw({ idAccount, value });
 
       res.status(200).json(transaction);
     } catch (e) {
       res.status(400).json({ msg: e.message });
     }
+  }
+
+  private validate(req: express.Request): TransactionInput {
+    const { value } = req.body;
+    const { idAccount } = req.params;
+    if (!idAccount || !value || typeof value !== 'number') throw new Error('Invalid Input');
+    const id = Number(idAccount);
+
+    return { idAccount: id, value };
   }
 }
 
