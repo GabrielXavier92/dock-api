4cb0e4d2a5f732ed5905abf76db0da32d361600f
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-18 12:22:39 -0300

    change account interface

--- src/app/entities/account/account.interface.d.ts
+++ src/app/entities/account/account.interface.d.ts
@@ -23,18 +23,19 @@ export interface AccountModelInput extends AccountInput {
 }
 export interface InterfaceAccountController {
   createAccount: (req: Request, res: Response) => Promise<void>;
-  // blockAccout: (req: Request, res: Response) => Promise<void>;
+  blockAccount: (req: Request, res: Response) => Promise<void>;
   // balanceAccount: (req: Request, res: Response) => Promise<void>;
 }
 
 export interface InterfaceAccountService {
   createAccount: (account: AccountInput) => Promise<Account>;
-  // blockAccout: (idAccount: number) => Promise<Account>;
+  blockAccount: (idAccount: number) => Promise<Account>;
   // balanceAccount: (idAccount: number) => Promise<Account>;
 }
 
 export interface InterfaceAccountModel {
-  create: (account: AccoutModel) => Promise<Account>;
-  // findById: (idAccount: number) => Promise<Account>;
+  create: (account: AccountModelInput) => Promise<Account>;
+  updateById: (id: number, account: AccountModelInput) => Promise<Account>;
+  findById: (idAccount: number) => Promise<Account | undefined>;
   // updateById: (idAccount: number, account: AccountInput) => Promise<Account>;
 }
