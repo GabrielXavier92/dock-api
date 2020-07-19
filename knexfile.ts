3104a6731df54d5da18a75c8e1664ece35bd8c1a
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-19 14:05:17 -0300

    change knex to use only full url

--- knexfile.ts
+++ knexfile.ts
@@ -3,12 +3,7 @@ import { knexSnakeCaseMappers } from 'objection';
 
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_FULL_URL || {
-    host: process.env.DATABASE_URL,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-  },
+  connection: process.env.DATABASE_FULL_URL,
   migrations: {
     extension: 'ts',
     directory: './src/database/migrations',
