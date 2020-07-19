150c2128d06f8a18344e0a86155a5cdbbcdd876a
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-19 18:00:14 -0300

    change connection if production mode

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import { knexSnakeCaseMappers } from 'objection';
 
+const production = process.env.NODE_ENV;
+
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_FULL_URL,
+  connection: production
+    ? {
+      host: 'db',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'dock',
+    } : process.env.DATABASE_FULL_URL,
   migrations: {
     extension: 'ts',
     directory: './src/database/migrations',
