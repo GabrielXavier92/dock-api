52ec265932b2bb8d57d6aaafaf0f81c9195b8202
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-19 17:14:19 -0300

    fix env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import App from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 const startSever = (): void => {
   App.app.listen(PORT, () => {
     console.log(`App is running in port ${PORT}`);
