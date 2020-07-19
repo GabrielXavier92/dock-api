e983937bfa7410802075e0837862e06bf2f5afd3
GabrielXavier92 <xavier_gabriel92@hotmail.com>
2020-07-19 13:26:25 -0300

    add query validation

--- src/app/entities/transaction/model/transactionModel.ts
+++ src/app/entities/transaction/model/transactionModel.ts
@@ -21,12 +21,20 @@ class TransactionModel implements InterfaceTransactionModel {
 
   public async findByIdAccount(idAccount: number, start?: string, end?: string): Promise<Array<Transaction> | undefined> {
     try {
+      if (start && end) {
+        const db = await connection.raw(`
+        SELECT * FROM "transaction" where "idAccount"=? AND "createdAt" BETWEEN ? AND ?
+      `, [`${idAccount}`, `${start}`, `${end}`]);
+
+        return db.rows as Array<Transaction>;
+      }
       const db = await connection.raw(`
-      SELECT * FROM "transaction" where "idAccount"=? AND "createdAt" BETWEEN ? AND ?
-    `, [`${idAccount}`, `${start}`, `${end}`]);
+        SELECT * FROM "transaction" where "idAccount"=?
+      `, [`${idAccount}`]);
 
       return db.rows as Array<Transaction>;
     } catch (e) {
+      console.log(e);
       throw new Error('Internal Error');
     }
   }
